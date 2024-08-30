425506d2496263ce9a1beb5821aeb4f0c586789d
Yan Meng <yannmm@foxmail.com>
2024-08-30 21:55:55 +0800

    part23 25.56

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::Base
   def redirect_to_username_form
     redirect_to edit_username_path(current_user)
   end
+
+  helper_method :previous_controller_name, :previous_action_name
+
+  def previous_controller_name
+    previous_path[:controller]
+  end
+
+  def previous_action_name
+    previous_path[:action]
+  end
+
+  def previous_path
+    @previous_path ||= Rails.application.routes.recognize_path(request.referrer)
+  end
 end
